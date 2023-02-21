f8b6b9c9cda9142c4fc47a461c58068acd9b21cf
crasride <jose.dfernandez@yahoo.fr>
2023-02-21 10:01:36 +0100

    is a file used to configure the admin interface

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book, Techno, Language, Author
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", )
 
 # Register your models here.
+admin.site.register(Book, BookAdmin)
+admin.site.register(Techno)
+admin.site.register(Language)
+admin.site.register(Author)
