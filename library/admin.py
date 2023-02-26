1fe33bb1c7aef55518cf5ad676b3b283a5bc0268
crasride <jose.dfernandez@yahoo.fr>
2023-02-26 21:09:56 +0100

    config panel administration django

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Techno, Language, Author
+from .models import Book, Techno, Language, Author, CheatSheet, Favorites
 
 class BookAdmin(admin.ModelAdmin):
     readonly_fields = ("created", )
@@ -9,3 +9,5 @@ admin.site.register(Book, BookAdmin)
 admin.site.register(Techno)
 admin.site.register(Language)
 admin.site.register(Author)
+admin.site.register(CheatSheet)
+admin.site.register(Favorites)
