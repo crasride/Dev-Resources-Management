05bfec86adff748653c9e73a04b56c035b682255
crasride <jose.dfernandez@yahoo.fr>
2023-02-27 16:27:52 +0100

    test deploy

--- system/settings.py
+++ system/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_DIRS = [BASE_DIR, 'static']
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 LOGIN_URL = '/signin'
 
 # Following settings only make sense on production and may break development environments.
