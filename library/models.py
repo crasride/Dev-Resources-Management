cbe1198de68cc91f1a59613b068362771c445c14
crasride <jose.dfernandez@yahoo.fr>
2023-02-28 17:16:40 +0100

    I check and add comments

--- library/models.py
+++ library/models.py
@@ -5,35 +5,42 @@ import datetime
 # Create your models here.
 
 
+# The Techno model with a name field and an auto-generated primary key
 class Techno(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+    # Return the name of the instance as a string
     def __str__(self):
         return self.name
 
-# sort data method
+    # Set the default ordering for instances of this model by name
     class Meta:
         ordering = ['name']
 
+# The Language model with a name field and an auto-generated primary key
 class Language(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+    # Return the name of the instance as a string
     def __str__(self):
         return self.name
 
+# The Author model with a name field and an auto-generated primary key
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, verbose_name='Author')
 
+    # Return the name of the instance as a string
     def __str__(self):
         return self.name
 
-    # sort data method
+    # Set the default ordering for instances of this model by name
     class Meta:
         ordering = ['name']
 
+# The CheatSheet model with several fields, including a foreign key to Techno and a file upload field
 class CheatSheet(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, verbose_name='Title')
@@ -43,10 +50,11 @@ class CheatSheet(models.Model):
     description = models.TextField(verbose_name='Description', blank=True)
     user = models.ForeignKey(User, on_delete=models.RESTRICT)
 
+    # Return the title and techno name of the instance as a string
     def __str__(self):
         return self.title + ' - ' + self.techno.name
 
-    # Allows local deletion of files
+    # Allow local deletion of files when an instance is deleted
     def delete(self, using=None, keep_parents=False):
         # Delete the image associated with the instance
         if self.link_imagen:
@@ -57,13 +65,16 @@ class CheatSheet(models.Model):
         # Call the delete method of the parent model to delete the instance
         super().delete(using=using, keep_parents=keep_parents)
 
+# The Favorites model with a foreign key to User and a many-to-many relationship with Book
 class Favorites(models.Model):
     user = models.ForeignKey(User, on_delete=models.RESTRICT, verbose_name='Favorites')
     books = models.ManyToManyField('Book', related_name='favorites')
 
+    # Return a string representation of the user's favorites
     def __str__(self):
         return f"{self.user}'s Favorites"
 
+# The Book model with several fields, including foreign keys to Techno and Language and an image upload field
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200, verbose_name='Title')
@@ -79,6 +90,7 @@ class Book(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.RESTRICT)
 
+    # Return a string representation of the user title
     def __str__(self):
         return self.title + '- by ' + self.user.username
     
@@ -86,10 +98,12 @@ class Book(models.Model):
     class Meta:
         ordering = ['techno']
 
-    # Allows local deletion of files
+    # Allow local deletion of files when an instance is deleted
     def delete(self, using=None, keep_parents=False):
+        # Delete the image associated with the instance
         if self.image:
             self.image.storage.delete(self.image.name)
+        # Delete the PDF file associated with the instance
         if self.pdf_link:
             self.pdf_link.storage.delete(self.pdf_link.name)
         super().delete(using=using, keep_parents=keep_parents)
