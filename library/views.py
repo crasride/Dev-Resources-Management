527fa3f1a089d1dbd65bca2999047274bcb0b39d
crasride <jose.dfernandez@yahoo.fr>
2023-02-28 18:27:01 +0100

    I check and add comments

--- library/views.py
+++ library/views.py
@@ -13,16 +13,19 @@ import requests
 
 # Create your views here.
 
-
+# This function handles requests to the home page of the website
 def home(request):
     return render(request, 'home.html')
 
+# This function handles requests to the about page of the website
 def about(request):
     return render(request, 'about.html')
 
+# This function handles requests to the cookies policy page of the website
 def cookies_policy(request):
     return render(request, 'cookies_policy.html')
 
+# This function handles requests to the contact page of the website
 def contact(request):
     return render(request, 'contact.html')
 
@@ -36,18 +39,30 @@ def search_books_online(request):
      the relevant data of the answer are extracted and if it does not give me
     an error.
     '''
+
+    # Get the search query from the request parameters
     query = request.GET.get('q')
+
+    # If the query is not empty
     if query:
+        # Build the search URL using the query parameter and a limit of 20 results
         url = f'http://openlibrary.org/search.json?q={query}&limit=20'
+        # Send an HTTP GET request to the Open Library API
         response = requests.get(url)
+
+        # If the request is successful (status code 200)
         if response.status_code == 200:
+            # Extract the list of books from the JSON response
             books = response.json().get('docs', [])
             # print(books)
+            # Render the search results page with the list of books and the query parameter
             return render(request, 'search_books_online.html', {'books': books, 'query': query})
         else:
+            # If the request fails, display an error message with the status code
             error_message = f'An error occurred while searching for books: {response.status_code}'
             return render(request, 'search_books_online.html', {'error': error_message, 'query': query})
     else:
+        # If the query is empty, just render the search page without results
         return render(request, 'search_books_online.html')
 
 # Definition of functions for interaction with the favorites table.(section)
@@ -86,7 +101,11 @@ def favorites_list(request):
 
 @login_required # Route Protection
 def book_detail_favorites(request, book_id):
+    # This function retrieves the details of a book and renders the book detail page
+
+    # Retrieve the book object with the given book_id, or return a 404 error page
     book = get_object_or_404(Book, id=book_id)
+    # Render the book detail page with the book object
     return render(request, 'book_detail_favorites.html', {'book': book})
 
 # Definition of functions for interaction with the books table.(section)
@@ -194,23 +213,31 @@ def author_form(request):
       the database, an error message is shown and if the data sent in the Form,
       before saving them in the database.
     '''
+
+    # If the request method is GET, render an empty author form
     if request.method == 'GET':
         return render(request, 'author_form.html', {'form': AuthorForm()})
     else:
+        # If the request method is POST, process the submitted form data
         form = AuthorForm(request.POST or None)
         if form.is_valid():
             name = form.cleaned_data.get('name')
+            # If an author with the same name already exists in the database, show an error message
             if Author.objects.filter(name=name).exists():
                 return render(request, 'author_form.html', {
                     'form': form,
-                    'error': 'A techno with this name already exists.'
+                    'error': 'A author with this name already exists.'
                 })
             else:
-                new_techno = form.save(commit=False)
-                new_techno.user = request.user
-                new_techno.save()
+                # If the form data is valid, create a new author object
+                new_author = form.save(commit=False)
+                # Set the user field of the new author object to the current user
+                new_author.user = request.user
+                # Save the new author object to the database
+                new_author.save()
                 return render(request, 'author_form.html', {'success': True})
         else:
+            # If the form data is invalid, show an error message
             return render(request, 'author_form.html', {
                 'form': form,
                 'error': 'Please provide valid data.'
@@ -253,19 +280,23 @@ def delete_cheats(request, id):
       Delete a cheatsheet from the database
     '''
     try:
+        # get the cheat sheet object with the given id and belonging to the current user
         cheats = get_object_or_404(CheatSheet, id=id, user=request.user)
     except CheatSheet.DoesNotExist:
+        # If cheatsheet does not exist, error message and redirect to the cheats page
         messages.error(request, 'Error: The CheatSheet does not exist')
         return redirect('cheats')
 
     if cheats.user == request.user:
+        # the user owns the cheatsheet, delete it and display a success message
         cheats.delete()
         messages.success(
             request, 'The CheatSheet has been successfully removed!', extra_tags='success')
     else:
+        # the user does not own the cheatsheet, display an error message
         messages.error(
             request, 'You do not have permission to delete this CheatSheet', extra_tags='danger')
-
+    # redirect the user to the cheats page
     return redirect('cheats')
 
 # Definition of functions for interaction with the create techno table.(section)
@@ -289,11 +320,15 @@ def techno_form(request):
                     'error': 'A techno with this name already exists.'
                 })
             else:
+                # If the form data is valid, create a new techno object
                 new_techno = form.save(commit=False)
+                # Set the user field of the new techno object to the current user
                 new_techno.user = request.user
+                # Save the new techno object to the database
                 new_techno.save()
                 return render(request, 'techno_form.html', {'success': True})
         else:
+            # If the form data is not valid, render the form with error messages
             return render(request, 'techno_form.html', {
                 'form': form,
                 'error': 'Please provide valid data.'
@@ -352,10 +387,12 @@ def signin(request):
     '''
 
     if request.method == 'GET':
+        # If the request is a GET request, render the login
         return render(request, 'signin.html', {
             'form': AuthenticationForm
         })
     else:
+        # Authenticate the user using the username and password provided in the POST
         user = authenticate(
             request, username=request.POST['username'], password=request.POST
             ['password'])
@@ -364,21 +401,12 @@ def signin(request):
         # if the username is not valid it gives an error
         if user is None:
             return render(request, 'signin.html', {
-                'form': AuthenticationForm,
+                'form': AuthenticationForm, # Pass the authentication form
+                # Add the error message
                 'error': 'Username or password is incorrect'
             })
         else:
-            login(request, user)
+            # user was authenticated successfully
+            login(request, user) # Log the user in
+            # Redirect the user to the home page (in this case, 'books')
             return redirect('books')
-'''
-class AddTechnoView(View):
-    def post(self, request):
-        form = TechnoForm(request.POST)
-        if form.is_valid():
-            techno_name = form.cleaned_data['name']
-            techno = Techno.objects.create(name=techno_name)
-            data = {'id': techno.pk, 'text': techno_name}
-            return JsonResponse({'success': True, 'data': data})
-        else:
-            return JsonResponse({'success': False, 'errors': form.errors})
-'''
